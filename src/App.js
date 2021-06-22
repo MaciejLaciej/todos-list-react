bae74b204d1105d54787375a7782adafcf3c7729
Maciej Łata <maciejcezarylata@gmail.com>
2021-06-22 23:16:27 +0200

    toggleHideDone added to js-working button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
+import { useState } from "react";
 
 const tasks = [
   { id: 1, content: "Zjeść", done: true },
@@ -13,6 +14,12 @@ const tasks = [
 const hideDone = true;
 
 function App() {
+const [hideDone, setHideDone] = useState(false);
+
+const toggleHideDone =() => {
+  setHideDone(hideDone => !hideDone);
+}
+
   return (
     <div>
       <Container>
@@ -26,7 +33,12 @@ function App() {
         <Section
           title="Lista zadań"
           body={<Tasks tasks={tasks} hideDone={hideDone} />}
-          extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} />}
+          extraHeaderContent={
+          <Buttons
+          tasks={tasks}
+          hideDone={hideDone}
+          toggleHideDone={toggleHideDone}
+          />}
         />
       </Container>
     </div >
